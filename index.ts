4c6fdbcf29aefa36b44c534e90e72ecb40daa886
asmajid <127208900+asmajid@users.noreply.github.com>
2023-07-31 19:47:46 +0100

    Empty Observable

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import { Observable } from 'rxjs';
+// Empty Observable
+const observable$ = new Observable<string>((subscriber) => {
+  console.log('Observable executed');
+});
+
+console.log('Before subscribe');
+observable$.subscribe((value) => console.log(value));
+console.log('After subscribe');
 
 //créer un observable en utilisant la classe Observable de RxJS qui émet trois valeurs de manière asynchrone
 //subscription est établi pour écouter ces valeurs et les afficher dans la console au fur et à mesure qu'elles sont émises
-const observable$ = new Observable<string>((subscriber) => {
+/*const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
   subscriber.next('test-1');
   setTimeout(() => subscriber.next('test-2'), 2000);
   setTimeout(() => subscriber.next('test-3'), 4000);
 });
+*/
 // const subscription = observable$.subscribe((value) => console.log(value));
 
 // setTimeout(() => {
@@ -15,11 +24,11 @@ const observable$ = new Observable<string>((subscriber) => {
 //   subscription.unsubscribe();
 // }, 2000);
 
-
-console.log('Subscription 1 starts');
+/*console.log('Subscription 1 starts');
 observable$.subscribe((value) => console.log('Subscription 1:', value));
 
 setTimeout(() => {
   console.log('Subscription 2 starts');
   observable$.subscribe((value) => console.log('Subscription 2:', value));
 }, 1000);
+*/
