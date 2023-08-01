82104792c6e582d4d1f42dc4fac838268dd10b5e
asmajid <127208900+asmajid@users.noreply.github.com>
2023-08-01 13:19:59 +0100

    Error notification

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { Observable } from 'rxjs';
 
-//Complete Notification
+//Error Notification
 const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben');
   setTimeout(() => {
     subscriber.next('Charlie');
-    subscriber.complete();
   }, 2000);
+  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
 
   return () => {
     console.log('Teardown');
@@ -18,9 +18,32 @@ const observable$ = new Observable<string>(subscriber => {
 console.log('Before subscribe');
 observable$.subscribe({
   next: value => console.log(value),
+  error: err => console.log(err.message),
   complete: () => console.log('Completed')
 });
 console.log('After subscribe');
+
+// //Complete Notification
+// const observable$ = new Observable<string>(subscriber => {
+//   console.log('Observable executed');
+//   subscriber.next('Alice');
+//   subscriber.next('Ben');
+//   setTimeout(() => {
+//     subscriber.next('Charlie');
+//     subscriber.complete();
+//   }, 2000);
+
+//   return () => {
+//     console.log('Teardown');
+//   };
+// });
+
+// console.log('Before subscribe');
+// observable$.subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log('Completed')
+// });
+// console.log('After subscribe');
 
 // //More next notifications
 // const observable$ = new Observable<string>((subscriber) => {
