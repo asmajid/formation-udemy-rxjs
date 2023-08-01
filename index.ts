c43e4ba2a9196bb5f8a110b035ec2fc4e7a04de2
asmajid <127208900+asmajid@users.noreply.github.com>
2023-08-01 16:43:55 +0100

    Map

--- index.ts
+++ index.ts
@@ -1,34 +1,54 @@
 import { Observable } from 'rxjs';
 // import { combineLatest, fromEvent } from 'rxjs';
-// import { ajax } from 'rxjs/ajax';
-import { filter } from "rxjs/operators";
+import { ajax } from 'rxjs/ajax';
+// import { filter } from "rxjs/operators";
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-//Filter 
-interface NewsItem {
-  category: 'Business' | 'Sports';
-  content: string;
-}
+//Map 
+const randomFirstName$ = ajax<any>(
+  'https://random-data-api.com/api/name/random_name'
+).pipe(map((ajaxResponse) => ajaxResponse.response.first_name));
 
-const newsFeed$ = new Observable<NewsItem>(subscriber => {
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'A' }), 1000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Sports', content: 'B' }), 3000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'C' }), 4000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Sports', content: 'D' }), 6000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'E' }), 7000);
-});
+const randomCapital$ = ajax<any>(
+  'https://random-data-api.com/api/nation/random_nation'
+).pipe(map((ajaxResponse) => ajaxResponse.response.capital));
 
-const sportsNewsFeed$ = newsFeed$.pipe(
-  filter(item => item.category === 'Sports')
+const randomDish$ = ajax<any>(
+  'https://random-data-api.com/api/food/random_food'
+).pipe(map((ajaxResponse) => ajaxResponse.response.dish));
+
+forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
+  ([firstName, capital, dish]) =>
+    console.log(`${firstName} is from ${capital} and likes to eat ${dish}.`)
 );
 
-newsFeed$.subscribe(
-  item => console.log(item)
-);
+// //Filter
+// interface NewsItem {
+//   category: 'Business' | 'Sports';
+//   content: string;
+// }
+
+// const newsFeed$ = new Observable<NewsItem>(subscriber => {
+//   setTimeout(() =>
+//     subscriber.next({ category: 'Business', content: 'A' }), 1000);
+//   setTimeout(() =>
+//     subscriber.next({ category: 'Sports', content: 'B' }), 3000);
+//   setTimeout(() =>
+//     subscriber.next({ category: 'Business', content: 'C' }), 4000);
+//   setTimeout(() =>
+//     subscriber.next({ category: 'Sports', content: 'D' }), 6000);
+//   setTimeout(() =>
+//     subscriber.next({ category: 'Business', content: 'E' }), 7000);
+// });
+
+// const sportsNewsFeed$ = newsFeed$.pipe(
+//   filter(item => item.category === 'Sports')
+// );
+
+// newsFeed$.subscribe(
+//   item => console.log(item)
+// );
 
 // //Combinelatest
 // const temperatureInput = document.getElementById('temperature-input');
